ebb5118ca97e96d1ef5bf93022141d799c13b3dc
SaiManideep00 <saimanideep00@gmail.com>
2024-10-01 11:35:14 -0400

    Initial commit

---
+++ src/test/java/org/example/sampledocker/SampleDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sampledocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
